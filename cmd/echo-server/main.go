94ae84c234260102ec0930dae6b6142acd4f2114
pxt1 <paul.taylor@equifax.com>
2023-10-04 14:05:46 +0000

    addition of chaining with new post handling

--- cmd/echo-server/main.go
+++ cmd/echo-server/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"crypto/tls"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
@@ -37,15 +43,34 @@ func main() {
 	// setup features
 	setupFeatures()
 
-	ctx := signalContext()
+	ctx := context.Background()
+	if !feature["nosignals"] {
+		ctx = signalContext()
+	}
 
-	err := http.ListenAndServe(
-		":"+port,
-		h2c.NewHandler(
-			http.HandlerFunc(handler),
-			&http2.Server{},
-		),
+	// setup handler
+	handl := h2c.NewHandler(
+		http.HandlerFunc(handler),
+		&http2.Server{},
 	)
+
+	if feature["otel"] {
+		// Set up OpenTelemetry.
+		serviceName := "echo-server"
+		serviceVersion := "0.0.1"
+		otelShutdown, err := setupOTelSDK(ctx, serviceName, serviceVersion)
+		if err != nil {
+			return
+		}
+		// Handle shutdown properly so nothing leaks.
+		defer func() {
+			err = errors.Join(err, otelShutdown(context.Background()))
+		}()
+
+		handl = otelhttp.NewHandler(handl, "")
+	}
+
+	err := http.ListenAndServe(":"+port, handl)
 	if err != nil {
 		panic(err)
 	}
@@ -62,11 +87,15 @@ func setupFeatures() {
 	// populates features map from environment variable
 	features := os.Getenv("ENABLE_FEATURES")
 	feature = make(map[string]bool)
+	feature["nosignals"] = ContainsI(features, "nosignals")
 	feature["delay"] = ContainsI(features, "delay")
+	feature["think"] = ContainsI(features, "think")
 	feature["headers"] = ContainsI(features, "headers")
 	feature["env"] = ContainsI(features, "env")
 	feature["meta"] = ContainsI(features, "meta")
 	feature["log"] = ContainsI(features, "log")
+	feature["otel"] = ContainsI(features, "otel")
+	feature["post"] = ContainsI(features, "post")
 }
 
 func ContainsI(a string, b string) bool {
@@ -158,12 +187,112 @@ func serveWebSocket(wr http.ResponseWriter, req *http.Request) {
 }
 
 func serveHTTP(wr http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" && feature["post"] {
+		servePOST(wr, req)
+	} else {
+		serveGET(wr, req)
+	}
+}
+
+type Chain struct {
+	URL []string `json:"chain"`
+}
+
+func servePOST(wr http.ResponseWriter, req *http.Request) {
+	// Example
+	// curl -XPOST http://localhost:8080/?headers -d '{ "chain": ["http://localhost:8080/?headers&think=300ms&delay=300ms", "http://localhost:8080/?headers&think=150ms&delay=2000ms"]}'
+
+	// inspect and parse body for a chain
+	reqBody, _ := io.ReadAll(req.Body)
+	chain := Chain{}
+	err := json.Unmarshal([]byte(reqBody), &chain)
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+
+	if len(chain.URL) == 0 {
+		// no chain so act like get
+		serveGET(wr, req)
+		return
+	}
+
+	// pop front of chain and prepare new chainBody
+	url := chain.URL[0]
+	chain.URL = chain.URL[1:]
+	chainBody, _ := json.Marshal(chain)
+
+	// output req
 	wr.Header().Add("Content-Type", "text/plain")
 	wr.WriteHeader(200)
 
 	host, err := os.Hostname()
 	if err == nil {
-		fmt.Fprintf(wr, "Request served by %s\n\n", host)
+		fmt.Fprintf(wr, "Request received by %s at %s\n\n", host, time.Now().Format(time.RFC3339Nano))
+	} else {
+		fmt.Fprintf(wr, "Server hostname unknown: %s\n\n", err.Error())
+	}
+
+	fmt.Fprintf(wr, "%s %s %s\n", req.Proto, req.Method, req.URL)
+	fmt.Fprintln(wr, "")
+
+	// output request headers if requested
+	if _, ok := req.URL.Query()["headers"]; ok && feature["headers"] {
+		fmt.Fprintf(wr, "Host: %s\n", req.Host)
+		for key, values := range req.Header {
+			for _, value := range values {
+				fmt.Fprintf(wr, "%s: %s\n", key, value)
+			}
+		}
+		fmt.Fprintln(wr, "")
+	}
+
+	fmt.Fprintf(wr, "%s\n", reqBody)
+	fmt.Fprintln(wr, "")
+
+	// think delay before chain link if requested
+	delays := req.URL.Query()["think"]
+	if len(delays) > 0 && feature["think"] {
+		if d, err := time.ParseDuration(delays[0]); err == nil {
+			time.Sleep(d)
+			fmt.Fprintf(wr, "Thinking for: %s\n\n", delays[0])
+		}
+	}
+
+	// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
+
+	// disable tls checks on http post calls
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	// call next link in chain
+	resp, err := client.Post(url, "application/json", bytes.NewReader(chainBody))
+	if err != nil {
+		fmt.Fprintf(wr, "chain call failed with %s\n", err)
+		fmt.Fprintln(wr, "")
+		return
+	}
+
+	// delay response if requested
+	delays = req.URL.Query()["delay"]
+	if len(delays) > 0 && feature["delay"] {
+		if d, err := time.ParseDuration(delays[0]); err == nil {
+			time.Sleep(d)
+			fmt.Fprintf(wr, "Delayed by: %s\n\n", delays[0])
+		}
+	}
+
+	io.Copy(wr, resp.Body)
+}
+
+func serveGET(wr http.ResponseWriter, req *http.Request) {
+	wr.Header().Add("Content-Type", "text/plain")
+	wr.WriteHeader(200)
+
+	host, err := os.Hostname()
+	if err == nil {
+		fmt.Fprintf(wr, "Request received by %s at %s\n\n", host, time.Now().Format(time.RFC3339Nano))
 	} else {
 		fmt.Fprintf(wr, "Server hostname unknown: %s\n\n", err.Error())
 	}
--- go.mod
+++ go.mod
@@ -4,5 +4,11 @@ go 1.16
 
 require (
 	github.com/gorilla/websocket v1.4.2
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.45.0
+	go.opentelemetry.io/otel v1.19.0
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v0.42.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.18.0
+	go.opentelemetry.io/otel/sdk v1.19.0
+	go.opentelemetry.io/otel/sdk/metric v1.19.0
 	golang.org/x/net v0.7.0
 )
